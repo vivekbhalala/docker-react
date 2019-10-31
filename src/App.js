e21d09c0ce5d0b39026b4e045f2ad0a5dcf51d60
Vivek Bhalala <vbhalala@marketo.com>
2019-10-31 16:08:41 -0700

    Testing Travis

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Vivek Bhalala
+          Vivek Bhalala, Portland
         </p>
         <a
           className="App-link"
