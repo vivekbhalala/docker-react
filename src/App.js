f990d10d7f3e454fb096279aeb73ed016a8dac45
Vivek Bhalala <vbhalala@marketo.com>
2019-11-01 14:40:28 -0700

    Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Vivek Bhalala, Portland Oregon
+          Vivek Bhalala, Portland
         </p>
         <a
           className="App-link"
